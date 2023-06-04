ae2c891d30a10c37fbde19fb67e4d11c376bbdd7
Jakub Staňo <ajax770sk@gmail.com>
2023-06-04 17:29:00 +0200

    Contacts

---
+++ app/ContactModule/Presenters/BasePresenter.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ContactModule\Presenters;
+
+class BasePresenter extends \App\Presenters\BasePresenter
+{
+
+}
---
+++ app/ContactModule/Presenters/ListPresenter.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ContactModule\Presenters;
+
+class ListPresenter extends BasePresenter
+{
+    public function actionDefault()
+    {
+        //
+    }
+}
