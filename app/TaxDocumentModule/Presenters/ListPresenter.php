cd53b20ae48293e751c727425f03d3b45552d78f
Jakub Staňo <ajax770sk@gmail.com>
2023-07-03 01:06:31 +0200

    PDF

--- app/TaxDocumentModule/Presenters/ListPresenter.php
+++ app/TaxDocumentModule/Presenters/ListPresenter.php
@@ -11,6 +11,8 @@ use Dompdf\Options;
 use Nette\Application\Responses\FileResponse;
 use Nette\Application\UI\ITemplateFactory;
 use Nette\Http\Response;
+use Nette\Mail\Message;
+use Nette\Mail\SendmailMailer;
 use Ublaboo\DataGrid\DataGrid;
 
 class ListPresenter extends BasePresenter
@@ -33,45 +35,54 @@ class ListPresenter extends BasePresenter
         }
 
         //create template
-        $template = $this->templateFactory->createTemplate();
-        // Variables
-        $template->taxDocument = $taxDocument;
-        $template->localeCode = $taxDocument->getLocaleCode();
-        $template->currencyCode = $taxDocument->getCurrencyCode();
+        $data = $this->generatePDF($taxDocument);
         //
-        $template->setFile(get_app_folder_path() . '/TaxDocumentModule/templates/List/pdf.latte');
-
-        //options
-        $options = new Options();
-        $options->setIsPhpEnabled(true);
-        $options->setIsFontSubsettingEnabled(true);
-        $options->setIsRemoteEnabled(true);
-        $options->set('isRemoteEnabled', true);
-
-        //render pdf with Dompdf
-        $dompdf = new Dompdf($options);
-        $dompdf->getCanvas()->get_page_count();
-//        $template = str_replace(['[PP]'], [$dompdf->getCanvas()->get_page_count()], $template);
-        $dompdf->loadHtml($template, 'UTF-8');
-        $dompdf->setPaper('A4');
-        $dompdf->render();
-
-        $stream = $dompdf->output();
-        $filename = sprintf('doklad-%s.pdf', $taxDocument->getNumber());
-        $filepath = 'data/' . $filename;
-        //
-        file_put_contents($filepath, $stream);
+        file_put_contents($data['filepath'], $data['stream']);
         // Response
         //Send file response
         $fileResponse = new FileResponse(
-            $filepath,
-            $filename,
+            $data['filepath'],
+            $data['filename'],
             'application/pdf',
             false);
 
         $this->sendResponse($fileResponse);
         //
         $this->terminate();
+    }
+
+    public function actionEmail($id)
+    {
+        /** @var TaxDocument|null $taxDocument */
+        $taxDocument = $this->em->getRepository(TaxDocument::class)->find((int)$id);
+
+        if ( ! $taxDocument) {
+            $this->error();
+        }
+
+        $pdfData = $this->generatePDF($taxDocument);
+
+        // Template
+        $template = $this->templateFactory->createTemplate();
+        // Variables
+        $template->taxDocument = $taxDocument;
+        //
+        $template->setFile(get_app_folder_path() . '/templates/email/tax-document.latte');
+
+        // Message
+        $message = new Message();
+        $message
+            ->setSubject('Doklad č. ' . $taxDocument->getNumber())
+            ->setHtmlBody((string) $template)
+            ->setFrom($taxDocument->getSupplierBillingAddress()->getEmail())
+            ->addTo($taxDocument->getSubscriberBillingAddress()->getEmail())
+            ->addAttachment($pdfData['filepath']);
+
+        $mailer = new SendmailMailer();
+        $mailer->send($message);
+
+        $this->flashMessage('E-mail bol úspešne odoslaný', 'success');
+        $this->redirect(':TaxDocument:List:default');
     }
 
     /********************************************************************************
@@ -131,6 +142,8 @@ class ListPresenter extends BasePresenter
         // Actions
         $grid->addAction('pdf', 'PDF', ':TaxDocument:List:pdf', ['id' => 'id'])
              ->setClass('btn btn-info btn-sm');
+        $grid->addAction('email', 'E-mail', ':TaxDocument:List:email', ['id' => 'id'])
+             ->setClass('btn btn-info btn-sm');
         $grid->addAction('edit', 'Upraviť', ':TaxDocument:Edit:', ['id' => 'id'])
              ->setIcon('pencil')
              ->setClass('btn btn-warning btn-sm');
@@ -144,5 +157,44 @@ class ListPresenter extends BasePresenter
 
 
         return $grid;
+    }
+
+    // --------------------------------------- Helpers ------------------------------------------------ \\
+
+    private function generatePDF(TaxDocument $taxDocument): array
+    {
+        //create template
+        $template = $this->templateFactory->createTemplate();
+        // Variables
+        $template->taxDocument = $taxDocument;
+        $template->localeCode = $taxDocument->getLocaleCode();
+        $template->currencyCode = $taxDocument->getCurrencyCode();
+        //
+        $template->setFile(get_app_folder_path() . '/TaxDocumentModule/templates/List/pdf.latte');
+
+        //options
+        $options = new Options();
+        $options->setIsPhpEnabled(true);
+        $options->setIsFontSubsettingEnabled(true);
+        $options->setIsRemoteEnabled(true);
+        $options->set('isRemoteEnabled', true);
+
+        //render pdf with Dompdf
+        $dompdf = new Dompdf($options);
+        $dompdf->getCanvas()->get_page_count();
+//        $template = str_replace(['[PP]'], [$dompdf->getCanvas()->get_page_count()], $template);
+        $dompdf->loadHtml($template, 'UTF-8');
+        $dompdf->setPaper('A4');
+        $dompdf->render();
+
+        $stream = $dompdf->output();
+        $filename = sprintf('doklad-%s.pdf', $taxDocument->getNumber());
+        $filepath = 'data/' . $filename;
+
+        return array(
+            'dompdf' => $dompdf,
+            'filename' => $filename,
+            'filepath' => $filepath
+        );
     }
 }
