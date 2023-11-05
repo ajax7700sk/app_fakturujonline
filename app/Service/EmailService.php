8c473460cf4134f3318ffd954342854c656be08a
Jakub Staňo <ajax770sk@gmail.com>
2023-11-05 21:45:54 +0100

    SMTP

--- app/Service/EmailService.php
+++ app/Service/EmailService.php
@@ -6,10 +6,10 @@ namespace App\Service;
 use App\Entity\TaxDocument;
 use App\Entity\User;
 use Nette\Application\UI\ITemplateFactory;
-use Nette\Forms\Validator;
 use Nette\Mail\Message;
 use Nette\Mail\SendException;
 use Nette\Mail\SendmailMailer;
+use Nette\Mail\SmtpMailer;
 use Nette\Utils\Validators;
 
 class EmailService
@@ -23,8 +23,8 @@ class EmailService
     /**
      * @param TaxDocument $taxDocument
      *
-     * @throws SendException
      * @return void
+     * @throws SendException
      */
     public function sendTaxDocument(TaxDocument $taxDocument): void
     {
@@ -37,11 +37,11 @@ class EmailService
         $template->setFile(get_app_folder_path().'/templates/email/tax-document.latte');
 
         // Validate
-        if(!Validators::isEmail($taxDocument->getSupplierBillingAddress()->getEmail())) {
+        if ( ! Validators::isEmail($taxDocument->getSupplierBillingAddress()->getEmail())) {
             throw new \InvalidArgumentException('Email odosielateľa nie je platný');
         }
 
-        if(!Validators::isEmail($taxDocument->getSubscriberBillingAddress()->getEmail())) {
+        if ( ! Validators::isEmail($taxDocument->getSubscriberBillingAddress()->getEmail())) {
             throw new \InvalidArgumentException('Email prijímateľa nie je platný');
         }
 
@@ -50,26 +50,27 @@ class EmailService
         $message
             ->setSubject('Doklad č. '.$taxDocument->getNumber())
             ->setHtmlBody((string)$template)
-            ->setFrom($taxDocument->getSupplierBillingAddress()->getEmail())
+            ->setFrom('no-reply@fakturujonline.sk')
+            ->addReplyTo($taxDocument->getSupplierBillingAddress()->getEmail())
             ->addTo($taxDocument->getSubscriberBillingAddress()->getEmail())
             ->addAttachment($pdfData['filepath']);
 
-        $mailer = new SendmailMailer();
+        $mailer = $this->getSMTPMailer();
         $mailer->send($message);
     }
 
     /**
      * @param TaxDocument $taxDocument
      *
-     * @throws SendException
      * @return void
+     * @throws SendException
      */
     public function resetPassword(User $user, string $resetLink): void
     {
         // Template
         $template = $this->templateFactory->createTemplate();
         // Variables
-        $template->user = $user;
+        $template->user      = $user;
         $template->resetLink = $resetLink;
         //
         $template->setFile(get_app_folder_path().'/templates/email/reset-password.latte');
@@ -82,13 +83,13 @@ class EmailService
             ->setFrom('no-reply@fakturujonline.sk')
             ->addTo($user->getEmail());
 
-        $mailer = new SendmailMailer();
+        $mailer = $this->getSMTPMailer();
         $mailer->send($message);
     }
 
     /**
-     * @throws SendException
      * @return void
+     * @throws SendException
      */
     public function registration(User $user): void
     {
@@ -107,7 +108,22 @@ class EmailService
             ->setFrom('no-reply@fakturujonline.sk')
             ->addTo($user->getEmail());
 
-        $mailer = new SendmailMailer();
+        $mailer = $this->getSMTPMailer();
         $mailer->send($message);
+    }
+
+    private function getBrevoSMTPMailer(): SmtpMailer
+    {
+        return new SmtpMailer(array(
+            'host'     => 'smtp-relay.brevo.com',
+            'username' => 'fakturujonline@gmail.com',
+            'password' => '2fvUZ0dg8rNSQsq5',
+            'port'     => 587,
+        ));
+    }
+
+    private function getSMTPMailer(): SmtpMailer
+    {
+        return $this->getBrevoSMTPMailer();
     }
 }
