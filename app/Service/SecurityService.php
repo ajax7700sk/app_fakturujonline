95fdabdf1215bf5f107f06cc038755d24f5e862c
Jakub Staňo <ajax770sk@gmail.com>
2023-11-05 21:17:25 +0100

    exc

--- app/Service/SecurityService.php
+++ app/Service/SecurityService.php
@@ -29,7 +29,7 @@ class SecurityService
         // Send some mail?
         try {
             $this->emailService->registration($user);
-        } catch (\Exception) {
+        } catch (\Exception $e) {
             //
         }
 
